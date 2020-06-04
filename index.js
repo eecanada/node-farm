95979d5df3390b5dad0d7144297b8de0111a6e45
Eder Canada <canada.eder@gmail.com>
2020-06-03 22:52:34 -0700

    readFile asynchronously

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 // fs is a node module
 const fs = require('fs')
 
+
+////////BLOCKING//////
 // takes two arguements, path to file I am reading and character encoding
 // reading a file
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 console.log(textIn)
 
-
-const textOut = `This is what we know about the avocado ${textIn}. \n Created on ${Date.now()}`
+const textOut = `This is what we know about the avocado ${textIn}. \nCreated on ${Date.now()}`
 //writing and creating file, takes two arguements path of file and what I want to put inside it 
 fs.writeFileSync('./txt/output.txt',textOut)
 console.log('File has been written')
 
+/////////NON-BLOCKING////////
+// data can be named anything I want it, it is the file start.txt
+fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
+  console.log(data)
+})
