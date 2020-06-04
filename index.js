472eef62bcd675ba2b4db343867e4cddb1cfe39e
Eder Canada <canada.eder@gmail.com>
2020-06-03 23:02:36 -0700

    append to files

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ console.log('File has been written')
 
 /////////NON-BLOCKING////////
 // data can be named anything I want it, it is the file start.txt
-fs.readFile('./txt/start.txt', 'utf-8', (err, data) => {
-  console.log(data)
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8',(err, data2)=>{
+    fs.readFile('./txt/append.txt','utf-8', (err, data3)=>{
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err =>{
+        console.log('your file has been written')
+      })
+    })
+  })
 })
