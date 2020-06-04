661774e72a8a79dd6de58e243a3fddb97940187f
Eder Canada <canada.eder@gmail.com>
2020-06-03 21:35:36 -0700

    write and create file

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 const fs = require('fs')
 
 // takes two arguements, path to file I am reading and character encoding
-const text = fs.readFileSync('./txt/input.txt', 'utf-8')
-console.log(text)
+// reading a file
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+console.log(textIn)
+
+
+const textOut = `This is what we know about the avocado ${textIn}. \n Created on ${Date.now()}`
+//writing and creating file, takes two arguements path of file and what I want to put inside it 
+fs.writeFileSync('./txt/output.txt',textOut)
+console.log('File has been written')
+
