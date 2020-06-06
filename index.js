586772c057bc4d175514e360cd630f49ca60f256
Eder Canada <canada.eder@gmail.com>
2020-06-06 16:38:16 -0700

    parse and send json to client

--- index.js
+++ index.js
@@ -8,16 +8,31 @@ const url = require('url')
 //////////////////////////////SERVER////////////////
 const server = http.createServer((req, res)=>{
   //sending response to client everytime a new request hits the server
-  console.log(req.url, 'hit') // /
+  console.log(req.url, 'hit') // -> /
   const pathName = req.url
   if(pathName === '/' || pathName === '/overview'){
     res.end('This is the OVERVIEW')
   }
   else if(pathName === '/product'){
     res.end('This is the PRODUCT')
+
+  } else if (pathName === '/api'){
+    fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data)=>{
+      //JSON.parse takes json code(string) and turns it into JS object
+      const productData = JSON.parse(data)
+      console.log(productData)
+      res.writeHead(200 , {
+        //when I send json it needs to have this header
+        'Content-type':'application/json'
+      })
+      res.end(data)
+    })
+   
+
   } else {
     // header always needs to be set before the response  
     res.writeHead(404, {
+      // http header is info of response I am sending back
       'Content-type':'text/html' 
     })
     res.end('<h1>Page not found</h1>')
