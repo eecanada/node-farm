7ec45c67e899ade95168fc631ec0c8f6c8e7fcae
Eder Canada <canada.eder@gmail.com>
2020-08-29 14:15:19 -0700

    listening on port 8000

--- index.js
+++ index.js
@@ -67,8 +67,12 @@ const http = require('http')
 //////////////////////////////////SERVER/////////////////////////////////// 
 
 //creating server
-http.createServer((req,res)=>{
+const server = http.createServer((req,res)=>{
   res.end('hello from the server')
 })
 
+//
+server.listen(8000, '127.0.0.1', ()=>{
+  console.log('listening to request on port 8000')
+})
 
