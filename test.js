a0bb369535bef5e110c845ac7218eb7a0205d4d5
Eder Canada <canada.eder@gmail.com>
2020-09-23 20:53:21 -0700

    dummy text

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
 const fs = require('fs')
 
-fs.readFile('./txt/read-this.txt', 'utf-8', (err, data)=>{
-  console.log(data)
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1)=>{
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2)=>{
+    console.log(data2)
+    fs.writeFile('./txt/dummyTxt.txt', `${data1}\n${data2}`, err =>{
+      console.log('hit')
+    })
+  })
 })
 
-console.log('hit')
